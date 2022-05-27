545b878d42ed62a58a80f03c6789bdd102dddf9f
rails new my_shopify_app <viotrip@gmail.com>
2022-05-27 00:49:13 -0400

    instantfork

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="flex justify-center">
       <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-10 md:text-4xl">
-        Quick News
+        InstantFork
       </h2>
       
     </div>
