d02353d1a86ccc90cc64e9c4c9633bfa48b37fd3
rails new my_shopify_app <viotrip@gmail.com>
2022-05-27 00:35:37 -0400

    color

--- components/Header.js
+++ components/Header.js
@@ -4,9 +4,7 @@ const Header = () => {
       <h2 className="font-raleway font-bold text-6xl text-primary pt-20 pb-10 md:text-4xl">
         Quick News
       </h2>
-      <h4 className="font-raleway font-regular text-1s text-primary pt-20 pb-10 md:text-.1s">
-        powered by Microsoft News
-      </h4>
+      
     </div>
   );
 };
