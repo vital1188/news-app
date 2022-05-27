9fe494fab84c1767b26789f5496e2425bd31b33b
rails new my_shopify_app <viotrip@gmail.com>
2022-05-27 00:50:07 -0400

    footer

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <div className="flex justify-center">
       <p className="text-primary pb-12 md:w-60 md:text-center">
-        Quick News - powered by Microsoft News
+        InstantFork - powered by Microsoft News
 {" "}
       </p>
     </div>
